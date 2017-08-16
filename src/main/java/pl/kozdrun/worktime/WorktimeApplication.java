ca53c57d42e1e35c377f567936d6b4f511294679
LAPTOP-N29M03SO\Radek <rkozdrun@gmail.com>
2017-08-16 22:27:04 +0200

    Initial commit

---
+++ src/main/java/pl/kozdrun/worktime/WorktimeApplication.java
@@ -0,0 +1,12 @@
+package pl.kozdrun.worktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktimeApplication.class, args);
+	}
+}
